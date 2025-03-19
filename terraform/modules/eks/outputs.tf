e6764893cfee34191169e841eb5d6eab194130c1
Alex Kulikov <alexeyqu@gmail.com>
2025-03-19 02:34:30 +0000

    fix: switch to terraform-aws-modules modules

--- terraform/modules/eks/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "eks_cluster_id" {
-  description = "EKS cluster ID"
-  value       = aws_eks_cluster.eks_cluster.id
-}
-
-output "eks_cluster_endpoint" {
-  description = "EKS cluster endpoint"
-  value       = aws_eks_cluster.eks_cluster.endpoint
-}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,7 @@
 variable "region" {
-  default = "eu-north-1"
+  description = "AWS region"
+  type        = string
+  default     = "eu-north-1"
 }
 
 variable "vpc_name" {
